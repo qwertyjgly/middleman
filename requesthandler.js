5bf5ef6db7c0f93792fa39a41e091ab5b602b63a
Nathan Scott <67497436+qwertyjgly@users.noreply.github.com>
2024-02-19 22:03:50 +1100

    Update requesthandler.js

--- requesthandler.js
+++ requesthandler.js
@@ -35,15 +35,15 @@ http.createServer( function (req, res) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', '*');
         res.setHeader('Access-Control-Allow-Headers', '*');
-        res.setHeader('Access-Control-Allow-Credentials', false);
+        res.setHeader('Access-Control-Allow-Credentials', true);
         res.setHeader('Referrer-Policy', 'no-referrer');
 
-        if (req.method === "OPTIONS") {
+        if (req.method === "OPTIONS" || 0 === 0) {
                 res.writeHead(200);
                 res.end();
                 return;
         }
-        if (req.method === "POST"){
+        if (1 === 0 && req.method === "POST") {
                 let body = "";
                 req.on("data", chunk => {
                         body += chunk.toString()
@@ -59,11 +59,11 @@ http.createServer( function (req, res) {
                                 console.log("body:");
                                 console.log(requestData);
 
-                                con.query(sqlQuery, function (err, result, fiel>
+                                con.query(sqlQuery, function (err, result, fields) {
                                         if (err) {
-                                                console.error("Error executing >
+                                                console.error("Error executing query:", err);
                                                 res.statusCode = 500;
-                                                res.end(JSON.stringify({respons>
+                                                res.end(JSON.stringify({response: "Error executing query: " + err.message}));
                                                 return;
                                         }
 
@@ -73,9 +73,9 @@ http.createServer( function (req, res) {
                                         res.end(JSON.stringify(queryresult));
                                 });
                         } catch(err) {
-                                console.error("Error processing request: ", err>
+                                console.error("Error processing request: ", err);
                                 res.statusCode = 500;
-                                res.end(JSON.stringify({response: "Error proces>
+                                res.end(JSON.stringify({response: "Error processing request: " + err.message}));
                         }
                 })
         };
