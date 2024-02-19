9cb558b5fa1d98e8892b0d74279e473598e914c9
Nathan Scott <67497436+qwertyjgly@users.noreply.github.com>
2024-02-19 22:52:53 +1100

    Update requesthandler.js

--- requesthandler.js
+++ requesthandler.js
@@ -47,8 +47,8 @@ http.createServer( function (req, res) {
         return;
     }
 
-        res.writeHead(200);
-        res.end();
-        return;
+    res.writeHead(200);
+    res.end();
+    return;
 }).listen(8080);
 
