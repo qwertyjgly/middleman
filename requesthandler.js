2d9ce7c283a082fa5dc993d8a6334f188659ea63
Nathan Scott <67497436+qwertyjgly@users.noreply.github.com>
2024-02-19 22:06:39 +1100

    Update requesthandler.js

--- requesthandler.js
+++ requesthandler.js
@@ -38,46 +38,8 @@ http.createServer( function (req, res) {
         res.setHeader('Access-Control-Allow-Credentials', true);
         res.setHeader('Referrer-Policy', 'no-referrer');
 
-        if (req.method === "OPTIONS" || 0 === 0) {
-                res.writeHead(200);
-                res.end();
-                return;
-        }
-        if (1 === 0 && req.method === "POST") {
-                let body = "";
-                req.on("data", chunk => {
-                        body += chunk.toString()
-                });
-                req.on("end", () => {
-                        console.log("body:");
-                        console.log(body);
-                        try{
-                                var requestData = JSON.parse(body);
-                                //var requestData = body;
-                                var sqlQuery = requestData.query;
-
-                                console.log("body:");
-                                console.log(requestData);
-
-                                con.query(sqlQuery, function (err, result, fields) {
-                                        if (err) {
-                                                console.error("Error executing query:", err);
-                                                res.statusCode = 500;
-                                                res.end(JSON.stringify({response: "Error executing query: " + err.message}));
-                                                return;
-                                        }
-
-                                        console.log("requested data:");
-                                        console.log(result)
-                                        queryresult = result;
-                                        res.end(JSON.stringify(queryresult));
-                                });
-                        } catch(err) {
-                                console.error("Error processing request: ", err);
-                                res.statusCode = 500;
-                                res.end(JSON.stringify({response: "Error processing request: " + err.message}));
-                        }
-                })
-        };
+        res.writeHead(200);
+        res.end();
+        return;
 }).listen(8080);
 
